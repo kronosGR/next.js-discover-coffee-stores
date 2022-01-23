c956690bf0062934a855db2c51ac1aa37902bb21
kronosGR <geo.elgeo@gmail.com>
2022-01-23 16:37:32 +0100

    fallback true and routers.isFallback

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,17 +1,41 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-const CoffeeStore = () => {
+import coffeeStoreData from '../../data/coffee-store.json';
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoreData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
+    fallback: true
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
+
+  if (router.isFallback){
+    return <div>Loading...</div>
+  }
   return (
     <div>
       <Link href='/'>
         <a>Back to home</a>
       </Link>
-      <Link href="/coffee-store/dynamic">
-        <a >Go to dynamic store</a>
+      <Link href='/coffee-store/dynamic'>
+        <a>Go to dynamic store</a>
       </Link>
-      Coffee Store Page {router.query.id}
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
 };
