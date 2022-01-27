04470efe0cf7427a2aff553682d743c8cd73d276
kronosGR <geo.elgeo@gmail.com>
2022-01-27 08:28:06 +0100

    create recod in airtable

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -9,17 +9,46 @@ console.log(table);
 
 const createCoffeeStore = async (req, res) => {
   if (req.method === 'POST') {
-    const findCoffeeStoreRecords = await table
-      .select({
-        filterByFormula: `id="0"`,
-      })
-      .firstPage();
+    try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="2"`,
+        })
+        .firstPage();
 
-    if (findCoffeeStoreRecords.length!==0) {
-      res.json(findCoffeeStoreRecords);
-    }
-    else {
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
 
+        const createRecords = await table.create([
+          {
+            fields:{
+              id:"3",
+              name:"test2",
+              address:"test2 address",
+              neighbourhood:'test2 ne',
+              voting: 2,
+              imgUrl: ''
+            }
+          }
+        ])
+        
+        const records = createRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+
+        res.json({message: 'create a record', records: records})
+      }
+    } catch (error) {
+      console.log('Error finding store', error)
+      res.staut(500).json({message: 'Error finding store', error})
     }
   } else {
     res.json({ message: 'Not GET' });
