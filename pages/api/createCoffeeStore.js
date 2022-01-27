0ea3916ae75492c30abddc7063d2806d0b3263ed
kronosGR <geo.elgeo@gmail.com>
2022-01-27 08:07:26 +0100

    added POST

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -4,3 +4,16 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 );
 
 const table = base ("coffee-stores")
+
+console.log(table)
+
+const createCoffeeStore = (req, res)=> {
+
+  if(req.method === 'POST'){
+    res.json({message:'ssss'})
+  } else {    
+    res.json({message:'Not GET'})
+  }
+}
+
+export default createCoffeeStore
