50285c1164ed59fbc8cdcbcfb811516016681a31
kronosGR <geo.elgeo@gmail.com>
2022-01-29 11:45:18 +0100

    finished getCoffeeStoreById

--- pages/api/getCoffeeStoreById.js
+++ pages/api/getCoffeeStoreById.js
@@ -1,8 +1,21 @@
-const getCoffeeStoreById = (req, res) => {
+import { getMinifiedRecords } from "../../lib/airtable";
+
+const getCoffeeStoreById = async (req, res) => {
   const { id } = req.query;
 
   try {
     if (id) {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="${id}"`,
+        })
+        .firstPage();
+
+      if (findCoffeeStoreRecords.length !== 0) {
+        res.json(getMinifiedRecords(findCoffeeStoreRecords));
+      } else {
+        res.json({ message: `id ${id} could not be found` });
+      }
       res.json({ message: `id is created ${id}` });
     } else {
       res.status(400).json({ message: 'Id is missing' });
